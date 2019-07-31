4624c4da9b18613f3701a1e582390c2f11e34a84
HueWoon Kim <kho0307@gmail.com>
2019-07-31 21:41:05 +0900

    Initial Commit

---
+++ realmDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmDB
+//
+//  Created by Hue Woon Kim on 31/07/2019.
+//  Copyright © 2019 Hue Woon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
